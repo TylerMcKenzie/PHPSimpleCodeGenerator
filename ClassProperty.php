693cc5e7b58a5bc9a2a1a73ec1830a940836832c
tylermckenzie <tylrmckenz@gmail.com>
2019-04-15 22:02:54 +0000

    update docs

--- ClassProperty.php
+++ ClassProperty.php
@@ -1,34 +1,62 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tmckenzie
+ * Created by Tyler Mckenzie
  * Date: 4/15/2019
- * Time: 12:42 PM
  */
 
 //include "./ClassAttribute.php";
 
+/**
+ * Class ClassProperty
+ */
 class ClassProperty extends ClassAttribute
 {
+	/**
+	 * @var mixed default value of the property
+	 */
 	protected $default_value;
 
+	/**
+	 * @var string value type of the property
+	 */
 	protected $type;
 
+	/**
+	 * Returns the default value of the property
+	 *
+	 * @return mixed default value
+	 */
 	public function getDefaultValue()
 	{
 		return $this->default_value;
 	}
 
+	/**
+	 * Sets property default value
+	 *
+	 * @param mixed $default default value to be set
+	 */
 	public function setDefaultValue($default)
 	{
 		$this->default_value = $default;
 	}
 
-	public function getType()
+
+	/**
+	 * Returns the type of the property
+	 *
+	 * @return string property type
+	 */
+	public function getType() : string
 	{
 		return $this->type;
 	}
 
+	/**
+	 * Sets the type of the property
+	 *
+	 * @param string $type property type
+	 */
 	public function setType(string $type)
 	{
 		$this->type = $type;
